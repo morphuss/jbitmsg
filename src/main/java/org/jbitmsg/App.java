1f86a0136837c94d33f4891d179c3f19cad89eb3
Stefan Langeland <morphuspam@gmail.com>
2013-07-25 15:01:31 +0200

    working

--- src/main/java/org/jbitmsg/App.java
+++ src/main/java/org/jbitmsg/App.java
@@ -1,14 +1,29 @@
 package org.jbitmsg;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-	System.out.println(org.bouncycastle.asn1.sec.SECNamedCurves.getByName("secp256k1"));
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(final String[] args) throws Exception {
+        String text = "Testkey";
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte[] sha1hash = new byte[40];
+        md.update(text.getBytes("UTF-8"), 0, text.length());
+        sha1hash = md.digest();
+        
+        BigInteger number = new BigInteger(1,sha1hash);
+        System.out.println(sha1hash[3]);
+        System.out.println(number.toString(16));
+        
+
+        System.out.println(org.bouncycastle.asn1.sec.SECNamedCurves.getByName("secp256k1").getCurve()
+                        .createPoint(BigInteger.ONE, BigInteger.TEN, true).hashCode());
+
     }
+
+
 }
