46f751691668d3e3d523319452a1e12c60d51011
Stefan Langeland <morphuspam@gmail.com>
2013-07-25 14:23:20 +0200

    moved

---
+++ src/main/java/org/jbitmsg/App.java
@@ -0,0 +1,14 @@
+package org.jbitmsg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	System.out.println(org.bouncycastle.asn1.sec.SECNamedCurves.getByName("secp256k1"));
+        System.out.println( "Hello World!" );
+    }
+}
